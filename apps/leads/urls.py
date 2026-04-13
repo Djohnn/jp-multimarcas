7356519a0678934490f2b19e2790c3c06d027b84
Johnn <johnndavid27@gmail.com>
2026-04-13 03:08:45 -0300

    feat(leads): adiciona rota lead_update_status nas urls

--- apps/leads/urls.py
+++ apps/leads/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.leads.views import LeadCreateView
+from apps.leads.views import LeadCreateView, LeadListView, LeadUpdateStatusView
 
 urlpatterns = [
+    path('', LeadListView.as_view(), name='lead_list'),
     path('new/', LeadCreateView.as_view(), name='lead_create'),
+    path('<int:pk>/status/', LeadUpdateStatusView.as_view(), name='lead_update_status'),
 ]
