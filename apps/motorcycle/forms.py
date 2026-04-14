72eaca1d3ccf14714952d3b2ae24cf15215e80c6
Johnn <johnndavid27@gmail.com>
2026-04-13 22:05:05 -0300

    refactor(motorcycle): atualiza MotorcycleModelForm com novos campos e validacoes

--- apps/motorcycle/forms.py
+++ apps/motorcycle/forms.py
@@ -5,10 +5,43 @@ from apps.motorcycle.models import Motorcycle, MotorcycleBrand
 class MotorcycleModelForm(ModelForm):
     class Meta:
         model = Motorcycle
-        fields = '__all__'
+        fields = [
+            'brand',
+            'model',
+            'version',
+            'type',
+            'engine_cc',
+            'factory_year',
+            'model_year',
+            'plate',
+            'color',
+            'fuel',
+            'transmission',
+            'mileage',
+            'sale_price',
+            'fipe_value',
+            'status',
+            'ipva_paid',
+            'licensed',
+            'single_owner',
+            'photo',
+            'bio',
+        ]
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year and factory_year < 1980:
+            self.add_error('factory_year', 'Não é possível cadastrar motos anteriores a 1980')
+        return factory_year
+
+    def clean_sale_price(self):
+        sale_price = self.cleaned_data.get('sale_price')
+        if sale_price and sale_price < 1000:
+            self.add_error('sale_price', 'Valor da moto deve ser maior que R$ 1.000')
+        return sale_price
 
 
 class MotorcycleBrandModelForm(ModelForm):
     class Meta:
         model = MotorcycleBrand
-        fields = '__all__'
+        fields = ['name']
