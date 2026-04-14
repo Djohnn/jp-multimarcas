b1b07afe762bf71589284a43d0e27661e13fea34
Johnn <johnndavid27@gmail.com>
2026-04-13 22:05:05 -0300

    refactor(motorcycle): atualiza views com sale_price, WhatsApp context e ordering correto

--- apps/motorcycle/views.py
+++ apps/motorcycle/views.py
@@ -1,8 +1,9 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.conf import settings
 
 from apps.motorcycle.models import Motorcycle, MotorcycleBrand
 from apps.motorcycle.forms import MotorcycleModelForm, MotorcycleBrandModelForm
@@ -24,50 +25,64 @@ class MotorcycleListView(ListView):
 
         ordering = {
             'model': 'model',
-            'price_asc': 'value',
-            'price_desc': '-value',
+            'price_asc': 'sale_price',
+            'price_desc': '-sale_price',
             'year_asc': 'factory_year',
             'year_desc': '-factory_year',
         }
         motorcycles = motorcycles.order_by(ordering.get(order, 'model'))
         return motorcycles
-    
+
+
 class MotorcycleDetailView(DetailView):
     model = Motorcycle
     template_name = 'motorcycle/motorcycle_detail.html'
     context_object_name = 'motorcycle'
-    
 
-@method_decorator(login_required(login_url='/account/login/'), name='dispatch')   
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        moto = self.object
+        context['whatsapp_number'] = settings.WHATSAPP_NUMBER
+        context['mensagem_whatsapp'] = (
+            f"Olá! Me chamo e tenho interesse na "
+            f"{moto.brand} {moto.model} "
+            f"{moto.factory_year}. "
+            f"Preço: R$ {moto.sale_price}. "
+            f"Pode me passar mais informações?"
+        )
+        return context
+
+
+@method_decorator(login_required(login_url='/account/login/'), name='dispatch')
 class MotorcycleCreateView(CreateView):
     model = Motorcycle
     form_class = MotorcycleModelForm
-    template_name = "motorcycle/motorcycle_form.html"
+    template_name = 'motorcycle/motorcycle_form.html'
     success_url = reverse_lazy('motorcycle_list')
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['brand_form'] = MotorcycleBrandModelForm()
         return context
-    
-    
+
+
 @method_decorator(login_required(login_url='/account/login/'), name='dispatch')
-@method_decorator(staff_member_required, name='dispatch')    
+@method_decorator(staff_member_required, name='dispatch')
 class MotorcycleBrandCreateView(CreateView):
     model = MotorcycleBrand
     form_class = MotorcycleBrandModelForm
     template_name = 'motorcycle/brand_form.html'
     success_url = reverse_lazy('motorcycle_create')
 
+
 @method_decorator(login_required(login_url='/account/login/'), name='dispatch')
 class MotorcycleUpdateView(UpdateView):
     model = Motorcycle
     form_class = MotorcycleModelForm
     template_name = 'motorcycle/motorcycle_update.html'
-    
+
     def get_success_url(self):
-        return reverse_lazy('motorcycle_detail', kwargs={'pk': self.object.pk})
+        return reverse('motorcycle_detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(login_required(login_url='/account/login/'), name='dispatch')
